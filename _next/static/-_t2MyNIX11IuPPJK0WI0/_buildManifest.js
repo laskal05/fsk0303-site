ac634e2b3191b58b90695563c08531d3906299d1
laskal05 <hrh.syn.mnmk.lb@gmail.com>
2022-07-06 17:58:18 +0900

    Delete _next/static directory

--- _next/static/-_t2MyNIX11IuPPJK0WI0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/628-a6e695a5af6167db.js","static/css/0bba276f54dc02ac.css","static/chunks/pages/index-28373359ae7d345e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
