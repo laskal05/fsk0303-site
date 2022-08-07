5ef37b56cc16c901a9114e0495d04e76b75ec06f
laskal05 <>
2022-08-07 15:03:33 +0000

    Update from https://github.com/laskal05/fsk0303/commit/d190b75dae7a16b49aa1911149c7898822bd9945

--- _next/static/mDxQOGLtiCXkW1ZYwHIF1/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/227-bb437b526d95c4cb.js","static/css/0bba276f54dc02ac.css","static/chunks/pages/index-243450f00650daf1.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
